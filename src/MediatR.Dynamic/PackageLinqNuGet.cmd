08c563e5963fc8ad821102f9a00375c630129956
Steven Rasmussen <steven.rasmussen@gmail.com>
2021-01-25 12:43:46 -0700

    Updated nuspec file.

--- src/MediatR.Dynamic/PackageLinqNuGet.cmd
+++ src/MediatR.Dynamic/PackageLinqNuGet.cmd
@@ -1,3 +1,3 @@
-set version=1.0.0-alpha.10
+set version=1.0.0-alpha.11
 dotnet build MediatR.Dynamic.csproj -c Release
 nuget.exe pack MediatR.Dynamic.nuspec -OutputDirectory "..\..\..\..\LocalNugetFeed" -Version %version% -Properties Configuration=Release
