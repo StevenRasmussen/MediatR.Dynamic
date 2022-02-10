bcff53d53cec99161a9b6803687aa157f2c8379f
Steven Rasmussen <steven.rasmussen@gmail.com>
2022-02-10 08:06:31 -0700

    Updated NuSpec to reflect v10 changes.

--- src/MediatR.Dynamic/PackageNuGet.cmd
+++ src/MediatR.Dynamic/PackageNuGet.cmd
@@ -1,3 +1,3 @@
-set version=1.0.0-alpha.11
+set version=10.0.0
 dotnet build MediatR.Dynamic.csproj -c Release
 nuget.exe pack MediatR.Dynamic.nuspec -OutputDirectory "..\..\..\..\LocalNugetFeed" -Version %version% -Properties Configuration=Release
