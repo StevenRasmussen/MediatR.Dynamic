a6c4331d5ba9d8ebd351e1345adf6925a90bb33e
Steven Rasmussen <steven.rasmussen@gmail.com>
2024-07-10 20:39:43 -0600

    Updated to target multiple frameworks. Updated to v12 MediatR.

--- src/MediatR.Dynamic/PackageNuGet.cmd
+++
@@ -1,3 +0,0 @@
-set version=10.0.0
-dotnet build MediatR.Dynamic.csproj -c Release
-nuget.exe pack MediatR.Dynamic.nuspec -OutputDirectory "..\..\..\..\LocalNugetFeed" -Version %version% -Properties Configuration=Release
